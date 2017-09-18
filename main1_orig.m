593d47686a55f60ea3bec2f3016e2cb1a16002cf
anhtu96 <songoku3496@gmail.com>
2017-09-18 20:59:16 +0700

    quite big changes

--- main1_orig.m
+++ main1_orig.m
@@ -1,16 +1,17 @@
+tic
 clc;
 clear;
-distance = [ 0      0.730 0.640 0.840 0.800 0.430 0.380 1.010;
-             0.730  0     0.710 1.040 0.500 0.300 0.540 0.470;
-             0.640  0.710 0     1.420 1.050 0.600 0.920  1.160;
-             0.840  1.040 1.420 0     0.740 0.950 0.570  0.900;
-             0.800  0.500 1.050 0.740 0     0.520 0.460  0.200
-             0.430  0.300 0.600 0.950 0.520 0     0.390  0.690;
-             0.380  0.540 0.920 0.570 0.460 0.390 0      0.660;
-             1.010  0.470 1.160 0.900 0.200 0.690 0.690  0];
+distance = [ 0      730 640 840 800 430 380 1010;
+             730  0     710 1040 500 300 540 470;
+             640  710 0     1420 1050 600 920  1160;
+             840  1040 1420 0     740 950 570  900;
+             800  500 1050 740 0     520 460  200
+             430  300 600 950 520 0     390  690;
+             380  540 920 570 460 390 0      660;
+             1010 470 1160 900 200 690 690  0];
 len = length(distance);
-popsize = 100;
-iteration = 500;
+popsize = 200;
+iteration = 50;
 distance_sum = zeros(popsize,1);
 p = zeros(popsize,1);
 pc = 0.9;
@@ -18,19 +19,20 @@ pm = 0.01;
 
  for i = 1:popsize
      pop(i,:) = randperm(len);
-     distance_sum(i) = total_dis(pop(i,:));
-     fitness(i,1) = 1/distance_sum(i);
  end
-p = fitness/sum(fitness);
-[~,best_factor] = min(distance_sum);
-best_distance = distance_sum(best_factor);
-best_path = pop(best_factor,:);
+best_distance = 0;
 
-crossover_mat = roulette_selection(p);
-for i = 1:popsize
-    parent(i,:) = pop(crossover_mat(i),:);
-end
+
 for cnt = 1:iteration
+    for i = 1:popsize
+        distance_sum(i) = total_dis(pop(i,:),distance);
+        fitness(i,1) = 1/distance_sum(i)^2;
+    end
+    p = fitness/sum(fitness);
+    selected_mat = roulette_selection(p);
+    for i = 1:popsize
+        parent(i,:) = pop(selected_mat(i),:);
+    end
     %crossover
     for i = 1:(popsize/2)
         offspring(i*2-1,:) = parent(i*2-1,:);
@@ -61,15 +63,16 @@ for cnt = 1:iteration
             offspring(i,rand_pos(1)) = offspring(i,rand_pos(2));
             offspring(i,rand_pos(2)) = tmp;
         end
-        distance_sum(i) = total_dis(offspring(i,:));
+        distance_sum(i) = total_dis(offspring(i,:),distance);
     end
     [total_dis_tmp position_tmp] = min(distance_sum);
-    if total_dis_tmp < best_distance
+    if ((total_dis_tmp < best_distance) || (best_distance == 0))
         best_distance = total_dis_tmp;
         best_path = offspring(position_tmp,:);
     end
-    parent = offspring;
-    parent(1,:) = best_path;
+    pop = offspring;
+    pop(1,:) = best_path;
 end
-disp('Best path is: '); disp(best_path);
-fprintf('Distance of that path: %fkm\n',best_distance);
+disp('Best path is: '); best_path
+fprintf('Distance of that path: %dkm\n',best_distance);
+toc
